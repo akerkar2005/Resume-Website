17959cbe7fbd838516807f10c63d6ce422c78d31
akerkar2005 <akerkar@purdue.edu>
2025-07-22 12:58:24 -0400

    Last working change; first working deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import TermIntro from './components/TermIntro';
 import Home from './components/Home';
